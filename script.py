84dc674f21858eeb3e9dfefab3b785d98fddd3f6
Aswinchand321 <88029112+Aswinchand321@users.noreply.github.com>
2021-07-31 10:06:08 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,60 +1,17 @@
 class Script(object):
 
-    START_MSG = """<b>Hy {},
-
-I'm an advanced filter bot with many capabilities!
-There is no practical limits for my filtering capacity :)
-
-See <i>/help</i> for commands and more details.</b>
+    START_MSG = """<b>ഹായ് ചെല്ലം ,വല്ല സഹായവും വേണമെങ്കിൽ ഹെൽപ്പിൽ ക്ലിക്ക് ചെയ്യ് എന്തായാലും ഇവിടം വരെ വന്നതല്ലേ വെറുതെ പോകണ്ട 🤭
+</b>
 """
 
 
-    HELP_MSG = """
-<i>Add me as admin in your group and start filtering :)</i>
-
-
-<b>Basic Commands;</b>
-
-/start - Check if I'm alive!
-/help - Command help
-/about - Something about me!
-
-
-<b>Filter Commands;</b>
-
-<code>/add name reply</code>  -  Add filter for name
-
-<code>/del name</code>  -  Delete filter
-
-<code>/delall</code>  -  Delete entire filters (Group Owner Only!)
-
-<code>/viewfilters</code>  -  List all filters in chat
-
-
-<b>Connection Commands;</b>
-
-<code>/connect groupid</code>  -  Connect your group to my PM. You can also simply use,
-<code>/connect</code> in groups.
-
-<code>/connections</code>  -  Manage your connections.
-
-
-<b>Extras;</b>
-
-/status  -  Shows current status of your bot (Auth User Only)
-
-/id  -  Shows ID information
-
-<code>/info userid</code>  -  Shows User Information. Use <code>/info</code> as reply to some message for their details!
-
-
-<b>© @TroJanzHEX</b>
+    Help_MSG = """കൗതുകം ലേശം കൂടുതലാണ് അല്ലേ... 🌹🤭
 """
 
 
-    ABOUT_MSG = """⭕️<b>My Name : TroJanz Filter Bot</b>
+    ABOUT_MSG = """⭕️<b>🌹എന്റെ പേര്🌹: അധോലോകം ടിന്റു</b>
 
-⭕️<b>Creater :</b> @TroJanzHEX    
+⭕️<b>Creater :</b> @Myfreak123   
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
